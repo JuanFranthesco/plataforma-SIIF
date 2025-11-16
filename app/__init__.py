f09c40563161141518bdcdcdc3aeb4e5ee4c1c30
__Maurilio__ <116044148+Maurilio0987@users.noreply.github.com>
2025-11-16 09:07:15 -0300

    corrigindo Indentação

--- app/__init__.py
+++ app/__init__.py
@@ -5,35 +5,35 @@ from .extensions import db, login_manager, migrate
 def create_app():
    app = Flask(__name__, static_folder='static', template_folder='templates')
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SECRET_KEY'] = 'aQ1!zW2@sX3#eD4$rF5%tG6^yH7&uJ8*iK9(oL0)pZ_c-Vb=n+M<,l.>'
+   app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+   app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+   app.config['SECRET_KEY'] = 'aQ1!zW2@sX3#eD4$rF5%tG6^yH7&uJ8*iK9(oL0)pZ_c-Vb=n+M<,l.>'
 
-    upload_path = os.path.join(app.root_path, 'static', 'uploads')
-    os.makedirs(upload_path, exist_ok=True)
-    app.config['UPLOAD_FOLDER'] = upload_path
-    db.init_app(app)
-    login_manager.init_app(app)
-    login_manager.login_view = 'auth.login'
-    login_manager.login_message_category = 'info'
+   upload_path = os.path.join(app.root_path, 'static', 'uploads')
+   os.makedirs(upload_path, exist_ok=True)
+   app.config['UPLOAD_FOLDER'] = upload_path
+   db.init_app(app)
+   login_manager.init_app(app)
+   login_manager.login_view = 'auth.login'
+   login_manager.login_message_category = 'info'
 
-    from app.models import User
-    
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
+   from app.models import User
+   
+   @login_manager.user_loader
+   def load_user(user_id):
+      return User.query.get(int(user_id))
 
-    from app.routes import main_bp
-    app.register_blueprint(main_bp)
+   from app.routes import main_bp
+   app.register_blueprint(main_bp)
 
-    from app.api import api
-    app.register_blueprint(api)
+   from app.api import api
+   app.register_blueprint(api)
 
-    from app.auth import auth_bp
-    app.register_blueprint(auth_bp)
+   from app.auth import auth_bp
+   app.register_blueprint(auth_bp)
 
-    with app.app_context():
-        from app import models
-        db.create_all()
+   with app.app_context():
+      from app import models
+      db.create_all()
 
-    return app
+   return app
