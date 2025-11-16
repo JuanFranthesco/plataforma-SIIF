20dbbf31aaf7c7b2e9f32ecae363d2c551e87b5d
Anderson  Costa <141959908+anderson1904@users.noreply.github.com>
2025-11-16 01:28:31 -0900

    Add name field to User model

--- app/models.py
+++ app/models.py
@@ -16,6 +16,7 @@ class User(db.Model, UserMixin):
     __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     matricula = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(100), nullable=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     is_admin = db.Column(db.Boolean, default=False)
