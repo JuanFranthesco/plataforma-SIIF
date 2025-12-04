219617b5769fb47b090b58b5e80139b7b148cf3e
JuanFranthesco <155860797+JuanFranthesco@users.noreply.github.com>
2025-12-04 00:52:41 -0300

    Create lista_proibida.py

---
+++ app/lista_proibida.py
@@ -0,0 +1,33 @@
+PALAVRAS_GLOBAIS = [
+    # --- GERAL / XINGAMENTOS ---
+    "puta", "puto", "merda", "merdinha", "caralho", "porra", "buceta", "boceta",
+    "cu", "cusao", "cuzao", "bosta", "fdp", "filho da puta", "filha da puta",
+    "pau no cu", "pau no cú", "cacete", "krl", "vsf", "tnc", "chupa", "vadia",
+    "vagabunda", "vagabundo", "arrombado", "arrombada", "piranha", "piranha",
+    "safada", "safado", "corno", "corna", "chifrudo", "imbecil", "idiota",
+    "retardado", "retardada", "mongol", "mongoloide", "burro", "burra",
+    "estupido", "estupida", "animal", "jumento", "besta", "otario", "otaria",
+    "trouxa", "lixo", "verme", "escroto", "escrota", "babaovo", "babaca",
+
+    # --- RACISMO / PRECONCEITO RACIAL ---
+    "macaco", "macaca", "macacada", "preto fedido", "preta fedida",
+    "ticao", "tiçao", "carvao", "senzala", "escravo", "escrava",
+    "neguinho", "neguinha", "krioulo", "crioulo", "crioula", "crioulo sujo",
+    "sujo", "imundo", "encardido", "branquelo", "branquela", "azedo",
+    "amarelo", "chines porco", "judeu porco", "nazista", "hitler", "suastica",
+    "supremacia", "white power", "sieg heil",
+
+    # --- HOMOFOBIA / TRANSFOBIA ---
+    "viado", "viadinho", "bicha", "bichinha", "boiola", "marica", "maricas",
+    "gayzinho", "traveco", "travesti", "tranny", "shemale", "sapatão", "sapatao",
+    "sapata", "cola velcro", "caminhoneira", "homossexualismo", "doenca",
+    "aberracao", "aberração", "ideologia de genero", "cura gay",
+
+    # --- ÓDIO / VIOLÊNCIA / CRIME ---
+    "matar", "suicidio", "suicidar", "morra", "morre", "estupro", "estuprar",
+    "estuprador", "pedofilo", "pedofilia", "abusador", "assassino",
+    "terrorista", "bomba", "massacre", "tiroteio", "degolar", "esfaquear",
+    
+    # --- VARIAÇÕES LEET (Para evitar b.u.l.l.a) ---
+    "p.u.t.a", "c.u", "f.d.p", "m.e.r.d.a"
+]
